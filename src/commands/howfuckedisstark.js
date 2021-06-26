77e51381062c526320d93feac786a8aa078c4a72
Chase Crayne <Chawork1234@gmail.com>
2021-06-26 03:34:00 -0500

    test

--- src/commands/howfuckedisstark.js
+++ src/commands/howfuckedisstark.js
@@ -3,6 +3,6 @@ import {Message, MessageCollector} from 'discord.js'
 module.exports = {
   name:'howfuckedisstark',
   async execute(message, messageArray){
-    message.channel.send('Pretty fucked bruv, his bot is kinda broke dick.');
+    message.channel.send('Pretty fucked bruv, his bot is kinda broke dick.test');
   }
 }
