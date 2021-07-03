94000bb966e5aa345c31df6d3ffb6cf8cae5dc80
Chase Crayne <Chawork1234@gmail.com>
2021-07-02 23:38:11 -0500

    top-level-await workaround 2.0

--- src/index.js
+++ src/index.js
@@ -34,25 +34,25 @@ for (const file of commandFiles) {
 client.once('ready', async () =>{
     //Initial login execution
     log.info('Client#ready -> Ready!');
-
-		function updateRich(){
+		const starkId = config.starkId;
+		async function updateRich(){
 			const starkUser = client.users.fetch(starkId);
 			const authToken = await Authenticator.getAccessToken(starkUser);
 			const isoDate = new Date().toISOString();
-		  const oldIsoDate = new Date(Date.now()-900000);
+			const oldIsoDate = new Date(Date.now()-900000);
 			const options = {
-		      // These properties are part of the Fetch Standard
-		      method: 'POST',
-		      headers: {
-		        authorization: `Bearer ${authToken}`
-		      },
-		      body: {
-		        startDate: oldIsoDate,
-		        endDate: isoDate,
-		      }
-		  };
+					// These properties are part of the Fetch Standard
+					method: 'POST',
+					headers: {
+						authorization: `Bearer ${authToken}`
+					},
+					body: {
+						startDate: oldIsoDate,
+						endDate: isoDate,
+					}
+			};
 			const response = await fetch(this.baseURL+'users/self/egvs', options);
-		  client.user.setActivity(`Stark is ${response.egvs[0].value}`);
+			client.user.setActivity(`Stark is ${response.egvs[0].value}`);
 		}
 		setInterval(updateRich(), 300000);
 
